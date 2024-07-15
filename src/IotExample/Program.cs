d71fb2481140a0f8718a9d1229120ffe57717aed
Roman Buldygin <froooks@gmail.com>
2024-07-15 20:34:30 +0300

    I2C works

--- src/IotExample/Program.cs
+++ src/IotExample/Program.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Iot.Device.Board;
+using Iot.Device.Graphics.SkiaSharpAdapter;
+
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -8,10 +11,12 @@ internal class Program
 {
     static void Main(string[] args)
     {
+        SkiaSharpAdapter.Register();
         var builder = Host.CreateApplicationBuilder(args);
 
         builder.Services
             .AddHostedService<Worker>()
+            .AddSingleton<Board>(Board.Create())
             .AddSystemd();
 
         builder.Logging
--- src/IotExample/Worker.cs
+++ src/IotExample/Worker.cs
@@ -1,31 +1,52 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using System.Device.Gpio;
+using System.Drawing;
+
+using Iot.Device.Board;
+using Iot.Device.Graphics;
+using Iot.Device.Graphics.SkiaSharpAdapter;
+using Iot.Device.Ssd13xx;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace IotExample;
 
 internal class Worker : BackgroundService
 {
+    private const int PinNum = 20; // PA20
     private readonly ILogger<Worker> _logger;
+    private readonly GpioController _pinController;
+    private readonly GpioPin _pin;
+    private readonly Ssd1306 _display;
 
     public Worker(
         ILogger<Worker> logger,
-        IHostApplicationLifetime appLifetime)
+        Board board)
     {
         _logger = logger;
-        appLifetime.ApplicationStopping.Register(OnStopping);
+        _pinController = board.CreateGpioController();
+        _pin = _pinController.OpenPin(PinNum, PinMode.Output);
+        var i2cBus = board.CreateOrGetI2cBus(0, [11, 12]);
+        var device = i2cBus.CreateDevice(0x3c);
+        _display = new Ssd1306(device, 128, 32);
+        _display.EnableDisplay(true);
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        var timer = new PeriodicTimer(TimeSpan.FromSeconds(10));
+        var fontSize = 25;
+        var font = "DejaVu Sans";
+
+        var timer = new PeriodicTimer(TimeSpan.FromMilliseconds(100));
         while (await timer.WaitForNextTickAsync(stoppingToken))
         {
+            _pin.Toggle();
             _logger.LogWarning("Timer tick");
-        }
-    }
 
-    private void OnStopping()
-    {
-        _logger.LogWarning("Stopping");
+            using var image = BitmapImage.CreateBitmap(128, 32, PixelFormat.Format32bppArgb);
+            image.Clear(Color.Black);
+            var drawingApi = image.GetDrawingApi();
+            drawingApi.DrawText(DateTime.Now.ToString("HH:mm:ss"), font, fontSize, Color.White, new Point(0, 0));
+            _display.DrawBitmap(image);
+        }
     }
 }
